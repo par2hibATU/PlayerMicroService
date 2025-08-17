1846ed4ca9185244dbcdd9fefd064310be6ed77c
par2hibATU <parthibbdcox@gmail.com>
2025-08-17 14:17:03 +0100

    Controller is created and constructor  injection is performed

--- src/main/java/com/par2hib/tictactoe/Controller/PlayerController.java
+++ src/main/java/com/par2hib/tictactoe/Controller/PlayerController.java
@@ -1,4 +1,21 @@
 package com.par2hib.tictactoe.Controller;
 
+
+import com.par2hib.tictactoe.Service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/player")
 public class PlayerController {
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService){
+        this.playerService = playerService;
+    }
+
+
+
 }
