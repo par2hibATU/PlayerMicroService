a2c561820e458cb55d155f5b51382c7c0e68bde5
par2hibATU <parthibbdcox@gmail.com>
2025-08-17 20:10:11 +0100

    Lombok version is not stable. Not working with this service, so removed and used the traditional getters and setters

--- src/main/java/com/par2hib/tictactoe/DTO/Player.java
+++ src/main/java/com/par2hib/tictactoe/DTO/Player.java
@@ -7,10 +7,41 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Players")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class Player {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
 
     String uniqueId;
     String name;
