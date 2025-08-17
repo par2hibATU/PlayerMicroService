7e50568174707676f17af0e8bbe0e37202775435
par2hibATU <parthibbdcox@gmail.com>
2025-08-17 18:43:15 +0100

    Controller functions and connection with service class is created successfully

--- src/main/java/com/par2hib/tictactoe/Controller/PlayerController.java
+++ src/main/java/com/par2hib/tictactoe/Controller/PlayerController.java
@@ -1,13 +1,17 @@
 package com.par2hib.tictactoe.Controller;
 
 
+import com.par2hib.tictactoe.DTO.Player;
 import com.par2hib.tictactoe.Service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/player")
+@RequestMapping("/players")
 public class PlayerController {
     private final PlayerService playerService;
 
@@ -16,6 +20,39 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
+    @PostMapping
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player){
+        Player newPlayer = playerService.addPlayer(player);
+        return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<Player>> getAllPlayers(){
+        return new ResponseEntity<>(playerService.getAllPlayers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/player/{uniqueId}")
+    public ResponseEntity<Player> getPlayerById(@PathVariable String uniqueId){
+        return new ResponseEntity<>(playerService.getPlayerById(uniqueId), HttpStatus.OK);
+    }
+
+    @GetMapping("/player/{name}")
+    public ResponseEntity<List<Player>> getPlayerByName(@PathVariable String name){
+        return new ResponseEntity<>(playerService.getPlayerByName(name), HttpStatus.OK);
+    }
+
+    @PutMapping("/player/{uniqueId}")
+    public ResponseEntity<Player> updatePlayer(@PathVariable String uniqueId, @RequestBody Player player){
+        return new ResponseEntity<>(playerService.updatePlayer(uniqueId, player), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/player/{uniqueId}")
+    public ResponseEntity<String> deletePlayer(@PathVariable String uniqueId){
+        String result = playerService.deletePlayer(uniqueId);
+        if(result.contains("Not Found")){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
 
 }
